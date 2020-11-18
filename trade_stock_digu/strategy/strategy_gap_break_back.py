8bb332b7e29d82c1f8a64678bcb53bebbcc369c1
[digu1216] <[digu1216@gmail.com]>
2020-11-18 21:14:40 +0800

    选股策略：突破日选股更新

--- trade_stock_digu/strategy/strategy_gap_break_back.py
+++ trade_stock_digu/strategy/strategy_gap_break_back.py
@@ -6,18 +6,28 @@ from strategy_base import StrategyBase
 from trade_stock_digu.data_service import DataServiceTushare
 from trade_stock_digu.convert_utils import string_to_datetime, time_to_str
 
-class StrategyGapBreak(StrategyBase):
+class StrategyGapBreakBack(StrategyBase):
     """
-    缺口突破策略：
-    1、中长期均线多头排列(ma30>ma60>ma120>ma250>ma500)
+    缺口突破回调买入策略：
+    1、中长期均线多头排列(ma30>ma60>ma120>ma250)
     2、股价最近一年未暴涨(high_250/low_250<max_times_in_year)
-    3、选股日当天有缺口突破 
+    3、最近5（10）天有缺口突破 days_gap
+    4、缺口价格在250日新高附近(0.1)
+    5、股价回调到缺口位置，缺口下端价格+2%以内
+    6、缺口上方最高价涨幅0.2
 
     待补充：
     """
-    days_break = 250    # 缺口突破的N日新高
+    days_gap = 10    # 股票运行在缺口之上运行天数
+    days_break = 500    # 缺口（准备）突破的N日新高
     n_years = 2     # 上市时间超过n_years年    
+    pct_near_gap = 0.06 #缺口大小百分：(突破日缺口上沿价格-缺口下沿价格)/缺口下沿价格    
     max_times_in_year = 4   #股票年内最大涨幅 high_250/low_250    
+    pct_high_break = 0.1    # 缺口下沿价格距离长期最高价格（days_break）的百分比 
+    pct_max = 0.4  # 缺口上的最大涨幅 （缺口上的最高价-缺口下沿价格）/缺口下沿价格
+    # def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
+    #     """"""
+    #     super().__init__()
 
     def __init__(self):
         """"""
@@ -40,25 +50,23 @@ class StrategyGapBreak(StrategyBase):
             if dic_stock_price is None:
                 # 排除选股日停牌的股票
                 continue   
-            days_break_high = 'high_' + str(self.days_break)
+            high_gap = 'high_' + str(self.days_gap)
+            low_gap = 'low_' + str(self.days_gap)
+            days_break_gap = 'high_' + str(self.days_break)
             try:
                 if dic_stock_price['high_250'] / dic_stock_price['low_250'] < self.max_times_in_year and \
                         dic_stock_price['ma_30'] > dic_stock_price['ma_60'] and \
                         dic_stock_price['ma_60'] > dic_stock_price['ma_120'] and \
-                        dic_stock_price['ma_120'] > dic_stock_price['ma_250'] and \
-                        dic_stock_price['ma_250'] > dic_stock_price['ma_500']:
-                    price_date_picked = ds_tushare.get_stock_price_info(ts_code, date_picked)
+                        dic_stock_price['ma_120'] > dic_stock_price['ma_250']:
+                    date_pre = ds_tushare.get_pre_trade_date(date_picked, self.days_gap)
+                    price_pre = ds_tushare.get_stock_price_info(ts_code, date_pre)
                     if dic_stock_price['ts_code'] == '000998_SZ':
                         a = 1
-                    # 选出当日长阳突破新高的股票 当日收盘价==high_days_break 
-                    # 当日涨幅>5%
-                    if price_date_picked['high'] == price_date_picked[days_break_high] \
-                        and price_date_picked['pct_chg'] > 8.0:
-                        lst_code_picked.append(dic_stock_price['ts_code'])  
-                    
-                    # 选出已经成功突破之后缺口加速的股票
-                    # if price_date_picked['low'] > price_date_picked[days_break_high]:
-                    #     lst_code_picked.append(dic_stock_price['ts_code'])                                
+                    if price_pre['high'] < dic_stock_price[low_gap] and \
+                        price_pre['high'] > price_pre[days_break_gap]*(1.0-self.pct_high_break) and \
+                            dic_stock_price['low'] < price_pre['high']*(1.0+self.pct_near_gap) and \
+                                dic_stock_price[high_gap]/price_pre['high'] < (1.0+self.pct_max):
+                        lst_code_picked.append(dic_stock_price['ts_code'])                    
             except:
                 self.logger.info('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
                 self.logger.info(dic_stock_price)
@@ -67,8 +75,8 @@ class StrategyGapBreak(StrategyBase):
 
 if __name__ == "__main__":
     ds_tushare = DataServiceTushare()
-    strategy = StrategyGapBreak()
-    print(strategy.pick_stock('20201105'))
+    strategy = StrategyGapBreakBack()
+    print(strategy.pick_stock('20200828'))
     # lst_trade_date = ds_tushare.get_trade_cal('20200101', '20200701')
     # cnt_loop = 0
     # for item_date in lst_trade_date:
